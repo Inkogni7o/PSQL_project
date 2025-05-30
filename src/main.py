0f92e42093caab12ce3a526ddcc5f68f8adc7832
Inkogni7o <ezykov25@gmail.com>
2025-05-30 11:05:14 +0300

    add logic for choosing tables

--- src/main.py
+++ src/main.py
@@ -17,6 +17,12 @@ connection = psycopg2.connect(dbname='Clinics', user=USER, password=PASSWD_DB, h
 connection.autocommit = True
 cursor = connection.cursor()
 
+cursor.execute("""
+    SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema = 'public'; """)
+tables = [i[0] for i in cursor.fetchall()]
+
 # создание и заполнение таблиц, создание триггера, индексов, процедуры
 create_all_tables(cursor)
 # create_trigger(cursor)
@@ -27,10 +33,23 @@ create_procedurs(cursor)
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def index(): 
+    return render_template("choice.html", tables=tables)
+
+@app.route('/tables')
+def display_tables():
     cursor.execute("SELECT * FROM Clinics")
     data = cursor.fetchall()
-    return render_template("main.html", data=data)
+    return render_template("tables2.html", data=data)
+
+@app.route('/display_table', methods=['POST'])
+def display_table():
+    table_name = request.form.get('table_name')
+    if table_name not in tables:
+        return redirect(url_for('index'))
+    cursor.execute(f"SELECT * FROM {table_name}")
+    data = cursor.fetchall()
+    return render_template("tables.html", table_name=table_name, data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
